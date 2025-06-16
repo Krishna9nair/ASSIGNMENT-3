8bb292eb36999c9dd67424a243b065ef41d64cfc
Krishna9nair <knair9843@gmail.com>
2025-06-16 17:54:39 +0530

    Add files via upload

---
+++ assignment 3 task 1.py
@@ -0,0 +1,7 @@
+
+num = int(input("Enter a number: "))
+factorial = 1
+for a in range(1, num + 1):
+    factorial = factorial * a
+print(f"The factorial of {num} is {factorial}")
+
---
+++ assignment 3.py
@@ -0,0 +1,6 @@
+from math import log,sin,sqrt
+num = int(input("Enter a number: "))
+print(f"The square root of {num} is {sqrt(num)}")
+print(f"The log of {num} is {log(num)}")
+print(f"The sin of {num} is {sin(num)}")
+
